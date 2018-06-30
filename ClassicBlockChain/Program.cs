81594b87070fe349b589f67c1a2f4fd7ecd4ab90
Icer <liangshuang@uchaindb.com>
2018-06-30 08:09:14 +0800

    Add project files.

---
+++ ClassicBlockChain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassicBlockChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
